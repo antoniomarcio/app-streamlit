fc078379ed58ff962f99cf76ced133db4c1360d2
antoniomarcio <a.marcio00@gmail.com>
2023-04-05 10:16:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if arquivo:
             st.json(loads(arquivo.read()))
         case 'image', _:
             st.image(arquivo)
-        case 'text', 'x-python':
+        case 'text', 'octect-stream':
             st.code(arquivo.read().decode())
         case 'text', 'csv':
             df = read_csv(arquivo).transpose()
